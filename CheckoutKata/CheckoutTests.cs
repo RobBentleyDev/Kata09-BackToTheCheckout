2d46e378d86387aa8cbebc7654fc7958513a7e0a
Rob Bentley <rob@robbentley.net>
2020-04-25 07:37:19 +0100

    added GivenSingleProduct_WhenScanned_ThenTotalIsPriceOfProduct

---
+++ CheckoutKata/CheckoutTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using FluentAssertions;
+using System;
+
+namespace CheckoutKata
+{
+    // https://github.com/davelobban/checkout-kata
+
+    public class CheckoutTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        // Total of a Single product
+        // Calculate the total price of all individual products
+        // Calculate the total price of all items, taking into account weekly offers
+
+        [Test]
+        public void GivenSingleProduct_WhenScanned_ThenTotalIsPriceOfProduct()
+        {
+            var product = new Product("A99", 50);
+
+            var checkout = new Checkout();
+
+            checkout.Scan(product);
+            
+            checkout.Total().Should().Be(50);
+        }
+    }
+
+    internal class Checkout
+    {
+        public Checkout()
+        {
+        }
+
+        internal void Scan(Product product)
+        {            
+        }
+
+        internal decimal Total()
+        {
+            return 50;
+        }
+    }
+
+    internal class Product
+    {
+        private string sku;
+        private int price;
+
+        public Product(string sku, int price)
+        {
+            this.sku = sku;
+            this.price = price;
+        }
+    }
+}
