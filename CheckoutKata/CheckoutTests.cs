b4d0a1f8cdeee695479f953e711620616664dbe2
Rob Bentley <rob@robbentley.net>
2020-04-26 16:26:04 +0100

    Refactor

--- CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutTests.cs
@@ -161,20 +161,6 @@ namespace CheckoutKata
         }
     }
 
-    internal class DiscountPricingStrategy
-    {
-        public DiscountPricingStrategy(string sku, int qualifyingQuantity, int discountGiven)
-        {
-            Sku = sku;
-            QualifyingQuantity = qualifyingQuantity;
-            DiscountGiven = discountGiven;
-        }
-
-        public string Sku { get; }
-        public int QualifyingQuantity { get; set; }
-        public int DiscountGiven { get; set; }
-    }
-
     internal class Checkout
     {
         private readonly DiscountPricingStrategy discountPricingStrategy;
---
+++ CheckoutKata/DiscountPricingStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckoutKata
+{
+    internal class DiscountPricingStrategy
+    {
+        public DiscountPricingStrategy(string sku, int qualifyingQuantity, int discountGiven)
+        {
+            Sku = sku;
+            QualifyingQuantity = qualifyingQuantity;
+            DiscountGiven = discountGiven;
+        }
+
+        public string Sku { get; }
+        public int QualifyingQuantity { get; set; }
+        public int DiscountGiven { get; set; }
+    }
+}
