109119f2f766d4b527928e62b723eb2add3ec71b
Rob Bentley <rob@robbentley.net>
2020-04-26 17:43:07 +0100

    Refactor

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    internal class Checkout
+    {
+        private readonly List<DiscountPricingStrategy> discountPricingStrategies;
+        private List<Product> scanned;
+
+        public Checkout(List<DiscountPricingStrategy> discountPricingStrategies)
+        {
+            scanned = new List<Product>();
+            this.discountPricingStrategies = discountPricingStrategies;
+        }
+
+        internal void Scan(Product product)
+        {
+            scanned.Add(product);
+        }
+
+        internal void Scan(Basket basket)
+        {
+            while(basket.HasItems)
+            {
+                Scan(basket.RemoveNext());
+            }
+        }
+
+        internal decimal Total()
+        {
+            var total = scanned.Sum(product => product.Price) - TotalDiscount();
+
+            return total;
+        }
+
+        private int TotalDiscount()
+        {
+            var totalDiscount = 0;
+
+            foreach (var pricingStrategy in discountPricingStrategies)
+            {
+                totalDiscount = totalDiscount + DiscountForStrategy(pricingStrategy);
+            }
+
+            return totalDiscount;
+        }
+
+        private int DiscountForStrategy(DiscountPricingStrategy pricingStrategy)
+        {
+            var discount = 0;
+
+            var productsForStrategy = scanned
+                .Where(product => product.Sku == pricingStrategy.Sku);
+
+            if (productsForStrategy.Count() == pricingStrategy.QualifyingQuantity)
+            {
+                discount = pricingStrategy.DiscountGiven;
+            }
+
+            return discount;
+        }
+    }
+}
--- CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutTests.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using FluentAssertions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CheckoutKata
 {
@@ -202,65 +201,6 @@ namespace CheckoutKata
         private DiscountPricingStrategy DiscountPricingStrategyForProductB15()
         {
             return new DiscountPricingStrategy("B15", 2, 15);
-        }
-    }
-
-    internal class Checkout
-    {
-        private readonly List<DiscountPricingStrategy> discountPricingStrategies;
-        private List<Product> scanned;
-
-        public Checkout(List<DiscountPricingStrategy> discountPricingStrategies)
-        {
-            scanned = new List<Product>();
-            this.discountPricingStrategies = discountPricingStrategies;
-        }
-
-        internal void Scan(Product product)
-        {
-            scanned.Add(product);
-        }
-
-        internal void Scan(Basket basket)
-        {
-            while(basket.HasItems)
-            {
-                Scan(basket.RemoveNext());
-            }
-        }
-
-        internal decimal Total()
-        {
-            var total = scanned.Sum(product => product.Price) - TotalDiscount();
-
-            return total;
-        }
-
-        private int TotalDiscount()
-        {
-            var totalDiscount = 0;
-
-            foreach (var pricingStrategy in discountPricingStrategies)
-            {
-                totalDiscount = totalDiscount + DiscountForStrategy(pricingStrategy);
-            }
-
-            return totalDiscount;
-        }
-
-        private int DiscountForStrategy(DiscountPricingStrategy pricingStrategy)
-        {
-            var discount = 0;
-
-            var productsForStrategy = scanned
-                .Where(product => product.Sku == pricingStrategy.Sku);
-
-            if (productsForStrategy.Count() == pricingStrategy.QualifyingQuantity)
-            {
-                discount = pricingStrategy.DiscountGiven;
-            }
-
-            return discount;
         }
     }
 }
